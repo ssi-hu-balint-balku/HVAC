170af2de5ad6849652ecd5f93a0b2eb7737d5957
zach_lysobey <zach@lysobey.com>
2015-04-13 16:12:50 -0400

    more work

--- src/main/java/EnvironmentController.java
+++ src/main/java/EnvironmentController.java
@@ -1,13 +1,11 @@
 /**
- * heating & cooling elements are across the room from the
- *   thermostat, so increasing the temperature requires heat
- *   + fan, decreasing it requires cool + fan, etc.
- *   • rules:
+ * heating & cooling elements are across the room from the thermostat, so
+ * increasing the temperature requires heat + fan,
+ * decreasing it requires cool + fan, etc.
+ *   rules:
  *   • best effort: keep the temperature between 65 and 75
- *   • hard rule: the fan can't run for 5 minutes after the
- *   heater is turned off
- *   • hard rule: the fan can't run for 3 minutes after the cooler
- *   is turned off
+ *   • hard rule: the fan can't run for 5 minutes after the heater is turned off
+ *   • hard rule: the fan can't run for 3 minutes after the cooler is turned off
  */
 public class EnvironmentController {
 
@@ -24,7 +22,18 @@ public class EnvironmentController {
      *   should call it, of course].
      */
     void tick() {
-        hvac.heat(true);
-        hvac.fan(true);
+        int temp = hvac.temp();
+        if (temp < 65) {
+            hvac.heat(true);
+            hvac.fan(true);
+        } else if (temp > 75) {
+            hvac.cool(true);
+            hvac.fan(true);
+        } else {
+            hvac.heat(false);
+            hvac.cool(false);
+            hvac.fan(false);
+        }
+
     }
 }
--- src/test/java/EnvironmentControllerTest.java
+++ src/test/java/EnvironmentControllerTest.java
@@ -1,5 +1,5 @@
-import junit.framework.TestCase;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EnvironmentControllerTest {
@@ -15,6 +15,38 @@ public class EnvironmentControllerTest {
         testTemp = 60;
         controller.tick();
         assertTrue(heatIsTurnedOn);
+        assertTrue(fanIsTurnedOn);
+    }
+
+    @Test
+    public void heat_and_fan_should_turn_off_if_its_a_good_temp() {
+        heatIsTurnedOn = true;
+        fanIsTurnedOn = true;
+        EnvironmentController controller = new EnvironmentController(new FakeHVAC());
+        testTemp = 70;
+        controller.tick();
+        assertFalse(heatIsTurnedOn);
+        assertFalse(fanIsTurnedOn);
+    }
+
+
+    @Test
+    public void cool_and_fan_should_turn_off_if_its_a_good_temp() {
+        coolIsTurnedOn = true;
+        fanIsTurnedOn = true;
+        EnvironmentController controller = new EnvironmentController(new FakeHVAC());
+        testTemp = 70;
+        controller.tick();
+        assertFalse(coolIsTurnedOn);
+        assertFalse(fanIsTurnedOn);
+    }
+
+    @Test
+    public void should_turn_on_cool_and_fan_if_its_too_hot() {
+        EnvironmentController controller = new EnvironmentController(new FakeHVAC());
+        testTemp = 80;
+        controller.tick();
+        assertTrue(coolIsTurnedOn);
         assertTrue(fanIsTurnedOn);
     }
 
