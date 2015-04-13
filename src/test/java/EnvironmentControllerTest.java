61b64d571a254426edd6e60f1d86a45bdbb7f719
zach_lysobey <zach@lysobey.com>
2015-04-13 15:43:11 -0400

    create test for environmental controller

---
+++ src/test/java/EnvironmentControllerTest.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class EnvironmentControllerTest extends TestCase {
+    @Test
+    public void Should_do_something() {
+        assertTrue(true);
+    }
+}
